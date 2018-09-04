c261c7d21624cc240fabf29366892d8d289189f2
Andrew Waters <waters@me.com>
2018-09-04 21:11:55 +0100

    Add a basic error page

---
+++ pages/_error.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Layout from '../components/Layout.js'
+
+const Error = (props) => (
+    <Layout>
+        <p>
+            {props.statusCode
+                ? `A ${props.statusCode} occurred on server`
+                : 'An error occurred on client'}
+        </p>
+    </Layout>
+)
+
+Error.getInitialProps = async function ({ res, err }) {
+    const statusCode = res ? res.statusCode : err ? err.statusCode : null;
+    return { statusCode }
+}
+
+export default Error;
